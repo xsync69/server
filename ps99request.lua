d0c56e158ee13cd9834de96df6cd756e21512841
xsync69 <145942743+xsync69@users.noreply.github.com>
2025-01-30 01:38:48 +0000

    Update ps99request.lua

--- ps99request.lua
+++ ps99request.lua
@@ -1,4 +1,6 @@
 print("Working.") 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xsync69/server/refs/heads/main/getdeposits.lua"))() -- This is for the server starter to get your place id and job id for people to join and make sure webhook is valid
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xsync69/server/refs/heads/main/ps99autoVrxl.lua"))() -- For Ps99 auto trade deposit and withdraw and mailbox
 print("Started Auto Deposits")
+
 --Start the auto deposits and send confirmation
